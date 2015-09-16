754b2595fe9c6905769d52cbb6bf81d955927b66
alx <ammartinez008@hotmail.com>
2015-09-16 17:27:06 -0400

    added simple hashset counter class

--- src/Counting/CardinalityHashCounter.java
+++ src/Counting/CardinalityHashCounter.java
@@ -1,5 +1,26 @@
 package Counting;
 
-public class CardinalityHashCounter {
+import java.util.HashSet;
+
+public class CardinalityHashCounter implements CounterStrategy{
+    HashSet set;
+
+    public CardinalityHashCounter() {
+        set = new HashSet();
+    }
+
+
+    public void add(int value) {
+        set.add(value);
+    }
+
+    public boolean contains(int value) {
+        return set.contains(value);
+    }
+
+    public int getCardinality() {
+        return set.size();
+    }
+
 
 }
